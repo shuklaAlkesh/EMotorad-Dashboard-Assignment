d8c0d48483d159bf9e7eece6a23bb519eb5be569
Alkesh shukla <114308478+shuklaAlkesh@users.noreply.github.com>
2025-02-24 18:15:07 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  //baseURL: 'http://localhost:5002/api',
+    baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
